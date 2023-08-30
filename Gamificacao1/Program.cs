9bece04152cf03307168fcb629b554a85ad28657
maycon0miotto <142221730+maycon0miotto@users.noreply.github.com>
2023-08-30 20:42:44 -0300

    Update Program.cs

--- Gamificacao1/Program.cs
+++ Gamificacao1/Program.cs
@@ -1,39 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using MRV.Modas;
-
-namespace MRV.Modas
-{
-    public class Program
-    {
-
-        static void Main(string[] args)
-        {
-            
-                    case "3":
-                        ClientesUI clienteUI = new();
-                        clienteUI.GerenciarClientes();
-                        break;
-                    case "4":
-                        VendaUI vendaUI = new();
-                        vendaUI.RealizarVenda();
-                        break;
-                    case "5":
-                        RelatorioUI relatorioUI = new();
-                        relatorioUI.MostrarRelatorioVendas();
-                        break;
-                    case "0":
-                        continuar = false;
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida!");
-                        break;
-                }
-                Console.WriteLine("Pressione qualquer tecla para continuar...");
-                Console.ReadKey();
-            } while (continuar);
-        }
-
+using 
+         
 
 
 
