021896d15599fdf93cf6fa7ec428ae2f38c0c43a
VitorMalacarne <vitorhenriquewm@gmail.com>
2023-08-30 20:41:48 -0300

    main

--- Gamificacao1/Program.cs
+++ Gamificacao1/Program.cs
@@ -9,29 +9,7 @@ namespace MRV.Modas
 
         static void Main(string[] args)
         {
-            bool continuar = true;
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("1 - Gerenciar produtos");
-                Console.WriteLine("2 - Gerenciar categorias");
-                Console.WriteLine("3 - Gerenciar clientes");
-                Console.WriteLine("4 - Realizar venda");
-                Console.WriteLine("5 - Mostrar relatório de vendas");
-                Console.WriteLine("0 - Sair");
-                Console.Write("Escolha uma opção: ");
-                string opcao = Console.ReadLine();
-
-                switch (opcao)
-                {
-                    case "1":
-                        ProdutoUI produtoUI = new();
-                        produtoUI.GerenciarProdutos();
-                        break;
-                    case "2":
-                        CategoriaUI categoriaUI = new();
-                        categoriaUI.MenuDeFuncionalidades();
-                        break;
+            
                     case "3":
                         ClientesUI clienteUI = new();
                         clienteUI.GerenciarClientes();
